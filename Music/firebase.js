3932499b2bcd66bca37c12a5efdb29648875d3a8
Amangeldi96 <hobbyplus312@gmail.com>
2025-10-15 22:45:26 +0600

    Update firebase.js

--- Music/firebase.js
+++ Music/firebase.js
@@ -1,1 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
+const firebaseConfig = {
+  apiKey: "ТВОЙ_API_KEY",
+  authDomain: "ТВОЙ_PROJECT_ID.firebaseapp.com",
+  projectId: "ТВОЙ_PROJECT_ID",
+  storageBucket: "ТВОЙ_PROJECT_ID.appspot.com",
+  messagingSenderId: "ТВОЙ_SENDER_ID",
+  appId: "ТВОЙ_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
