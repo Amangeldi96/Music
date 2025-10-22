f0c8b0de43865a53fc3624395d271f922d822b0a
Amangeldi96 <193253467+Amangeldi96@users.noreply.github.com>
2025-10-22 23:34:18 +0600

    Обновил файл после редактирования

--- src/SupabaseClient.js
+++ src/SupabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://dboarfesletxtvyjrhos.supabase.co'; // ← твой URL
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRib2FyZmVzbGV0eHR2eWpyaG9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjExNDM4NTEsImV4cCI6MjA3NjcxOTg1MX0.UQm6jfPlPuUm2EEB9WfAiP3-M6ZKJUEjnln_P3fivl0';
+const supabaseUrl = 'https://iqvkeeegqhlibdjmjrdm.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlxdmtlZWVncWhsaWJkam1qcmRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjExNDcxOTksImV4cCI6MjA3NjcyMzE5OX0.2ErdtSBX_t8y2RFmImcsqhbPa7NCB80Zvkc8uP9e4kI';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
