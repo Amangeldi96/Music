fdf4517f88eba6e20022629a707932130c51a79b
Amangeldi96 <193253467+Amangeldi96@users.noreply.github.com>
2025-10-22 23:10:32 +0600

    Обновил файл после редактирования

--- src/SupabaseClient.js
+++ src/SupabaseClient.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://dboarfesletxtvyjrhos.supabase.co'; // ← твой URL
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; // ← твой ключ
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRib2FyZmVzbGV0eHR2eWpyaG9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjExNDM4NTEsImV4cCI6MjA3NjcxOTg1MX0.UQm6jfPlPuUm2EEB9WfAiP3-M6ZKJUEjnln_P3fivl0';
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
