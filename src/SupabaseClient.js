3eb0c88a0855833352c0519138e514e52fef976a
Amangeldi96 <193253467+Amangeldi96@users.noreply.github.com>
2025-10-22 22:54:52 +0600

    Обновил файл после редактирования

--- src/SupabaseClient.js
+++ src/SupabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project.supabase.co';
-const supabaseKey = 'your-anon-key';
+const supabaseUrl = 'https://dboarfesletxtvyjrhos.supabase.co'; // ← твой URL
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; // ← твой ключ
 export const supabase = createClient(supabaseUrl, supabaseKey);
